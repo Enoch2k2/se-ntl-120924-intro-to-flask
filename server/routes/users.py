c6f2ab43c793379edc74567a58ca5d8beeaff94d
Enoch2k2 <enoch2k2@hotmail.com>
2025-02-26 12:51:47 -0600

    finished demo

---
+++ server/routes/users.py
@@ -0,0 +1,9 @@
+from flask import jsonify
+from config import app
+from models.models import User
+from .helpers import *
+
+@app.route("/api/users")
+def users_route():
+  users = [user.to_dict() for user in User.query.all()]
+  return jsonify(users), 200
